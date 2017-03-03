ec1a299cc62a2ae42caf558d418bccf9f9d890d3
ejarvi <ejarvi@microsoft.com>
2017-03-03 11:50:02 -0800

    simplify and support running from service principal context

--- test/commands/arm/vm/arm.vm-disk-encryption-linux.js
+++ test/commands/arm/vm/arm.vm-disk-encryption-linux.js
@@ -21,67 +21,76 @@ var util = require('util');
 var profile = require('../../../../lib/util/profile');
 var testUtils = require('../../../util/util');
 var CLITest = require('../../../framework/arm-cli-test');
-var testprefix = 'arm-cli-vm-disk-encryption-extension-tests';
-var groupPrefix = 'xplatTestADE';
 var VMTestUtil = require('../../../util/vmTestUtil');
+
+/* precreated AD app environment variables allow the
+ * test to run from context of the service principal 
+ * which does not have the ability to create AD apps 
+ */
 var requiredEnvironment = [{
   name: 'AZURE_VM_TEST_LOCATION',
   defaultValue: 'westus'
+}, {
+  name: 'ADE_ADAPP_NAME',
+  defaultValue: 'xplatTestAdApp'
+}, {
+  name: 'ADE_ADAPP_SECRET',
+  defaultValue: ''
+}, {
+  name: 'ADE_ADSP_APPID',
+  defaultValue: ''
+}, {
+  name: 'ADE_ADSP_OID',
+  defaultValue: ''
 }, {
   name: 'SSHCERT',
   defaultValue: 'test/myCert.pem'
 }];
 
-var groupName,
-  vmPrefix = 'xplattestvm',
-  nicName = 'xplattestnic',
-  vaultName = 'xplattestkv',
-  aadAppName = 'xplattestapp',
-  aadClientSecret = 'xPL4t',
-  aadClientId,
-  identifierUri,
-  aadObjectId,
-  spn,
-  spObjectId,
-  location,
-  username = 'azureuser',
-  password = 'AzDE@2016',
-  storageAccount = 'xplatteststorage1',
-  storageCont = 'xplatteststoragecnt1',
-  vNetPrefix = 'xplattestvnet',
-  subnetName = 'xplattestsubnet',
-  publicipName = 'xplattestip',
-  dnsPrefix = 'xplattestipdns',
+var testprefix = 'arm-cli-vm-disk-encryption-linux';
+  groupPrefix = 'xplatTestRG',
+  groupName,
+  vmPrefix = 'xplatTestVM',
+  vmName,
+  adminUserPrefix = 'xptadmin',
+  adminUsername,
+  imageUrn = 'Ubuntu-LTS:latest',
+  vaultName = 'xplatTestKV',
+
+  adAppName,
+  adAppClientSecret,
+  adServicePrincipalAppId,
+  adServicePrincipalObjectID,
+
   subscriptionId,
   diskEncryptionKeyVaultId,
-  diskEncryptionKeySecretUrl,
-  sshcert,
-  winImageUrn = 'MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest';
+  diskEncryptionKeyVaultUrl,
+  sshPublicKeyFile;
 
-describe('arm', function() {
-  describe('compute', function() {
+describe('arm', function () {
+  describe('compute', function () {
     var suite, retry = 5;
     var vmTest = new VMTestUtil();
-    before(function(done) {
+    before(function (done) {
       this.timeout(vmTest.timeoutLarge);
       suite = new CLITest(this, testprefix, requiredEnvironment);
-      suite.setupSuite(function() {
+      suite.setupSuite(function () {
+        // resource group and virtual machine 
         location = process.env.AZURE_VM_TEST_LOCATION;
-        sshcert = process.env.SSHCERT;
         groupName = suite.generateId(groupPrefix, null);
-        vmPrefix = suite.isMocked ? vmPrefix : suite.generateId(vmPrefix, null);
-        nicName = suite.isMocked ? nicName : suite.generateId(nicName, null);
-        vaultName = suite.isMocked ? vaultName : suite.generateId(vaultName, null);
-        aadAppName = suite.isMocked ? aadAppName : suite.generateId(aadAppName, null);
-        identifierUri = util.format('http://localhost:8080/%s', aadAppName);
-        aadClientSecret = suite.isMocked ? aadClientSecret : suite.generateId(aadClientSecret, null);
-        storageAccount = suite.generateId(storageAccount, null);
-        storageCont = suite.generateId(storageCont, null);
-        vNetPrefix = suite.isMocked ? vNetPrefix : suite.generateId(vNetPrefix, null);
-        subnetName = suite.isMocked ? subnetName : suite.generateId(subnetName, null);
-        publicipName = suite.isMocked ? publicipName : suite.generateId(publicipName, null);
-        dnsPrefix = suite.generateId(dnsPrefix, null);
-        subscriptionId = profile.current.getSubscription().id;
+        vmName = suite.isMocked ? vmName : suite.generateId(vmPrefix, null);
+        adminUsername = suite.GenerateId(adminUserPrefix, null);
+        sshPublicKeyFile = process.env.SSHCERT;
+
+        // active directory (precreated to facilitate automation from service principal context)
+        adAppName = process.env.ADE_ADAPP_NAME,
+          adAppClientSecret = process.env.ADE_ADAPP_SECRET,
+          adServicePrincipalAppId = process.env.ADE_ADSP_APPID,
+          adServicePrincipalObjectID = process.env.ADE_ADSP_OID,
+
+          // key vault
+          vaultName = suite.isMocked ? vaultName : suite.generateId(vaultName, null);
+
         if (!suite.isPlayback()) {
           setupPrerequisites(done);
         } else {
@@ -90,7 +99,7 @@ describe('arm', function() {
       });
     });
 
-    after(function(done) {
+    after(function (done) {
       suite.teardownSuite(function () {
         if (!suite.isPlayback()) {
           cleanupCreatedArtifacts(done);
@@ -100,126 +109,56 @@ describe('arm', function() {
       });
     });
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
       suite.setupTest(done);
     });
 
-    afterEach(function(done) {
+    afterEach(function (done) {
       suite.teardownTest(done);
     });
 
-    function setupPrerequisites(done)
-    {
-      // Create a resource group
+    function setupPrerequisites(done) {
+      // create temporary resource group for test 
       suite.execute('group create -n %s -l %s --json', groupName, location, function (result) {
         result.exitStatus.should.equal(0);
-        // Create a Windows VM
-        suite.execute('vm create %s %s %s Windows -f %s -Q %s -u %s -p %s -o %s -R %s -F %s -P %s -j %s -k %s -i %s -w %s --json', groupName, vmPrefix, location, nicName, winImageUrn, username, password, storageAccount, storageCont, vNetPrefix, '10.4.0.0/16', subnetName, '10.4.0.0/24', publicipName, dnsPrefix, function (result) {
+        // quick create vm within the resource group 
+        suite.execute('vm quick-create --resource-group "%s" --name "%s" --admin-username "%s" --location "%s" --os-type Linux --image-urn "%s" --ssh-public-key-file "%s"', groupName, vmName, adminUsername, location, imageUrn, sshPublicKeyFile, function (result) {
           result.exitStatus.should.equal(0);
-          // Create an AAD app
-          suite.execute('ad app create -n %s --home-page "http://www.contoso.com" -i %s -p %s --json', aadAppName, identifierUri, aadClientSecret, function (result) {
+          // create keyvault 
+          suite.execute('keyvault create --vault-name "%s" --resource-group "%s" --location "%s" --json', vaultName, groupName, location, function (result) {
             result.exitStatus.should.equal(0);
-            var appResources = JSON.parse(result.text);
-            aadClientId = appResources.appId;
-            aadObjectId = appResources.objectId;
-            // Create a service principal
-            suite.execute('ad sp create --applicationId %s --json', aadClientId, function (result) {
+            var kvResources = JSON.parse(result.text);
+            diskEncryptionKeyVaultId = kvResources.id;
+            diskEncryptionKeyVaultUrl = kvResources.properties.vaultUri;
+            // set keyvault policy to enable disk encryption 
+            suite.execute('keyvault set-policy --vault-name "%s" --spn "%s" --perms-to-keys ["wrapKey"] --perms-to-secrets ["set"] --enabled-for-disk-encryption true --json', vaultName, adServicePrincipalAppId, function (result) {
               result.exitStatus.should.equal(0);
-              var spResources = JSON.parse(result.text);
-              spn = spResources.appId;
-              spObjectId = spResources.objectId;
-              // Create a KeyVault
-              suite.execute('keyvault create %s --resource-group %s --location %s --json', vaultName, groupName, location, function (result) {
-                result.exitStatus.should.equal(0);
-                var kvResources = JSON.parse(result.text);
-                diskEncryptionKeyVaultId = kvResources.id;
-                diskEncryptionKeySecretUrl = kvResources.properties.vaultUri;
-                // Prepare the vault for encryption
-                suite.execute('keyvault set-policy -u %s --spn %s --perms-to-keys ["all"] --perms-to-secrets ["all"] --enabled-for-disk-encryption true --json', vaultName, spn, function (result) {
-                  result.exitStatus.should.equal(0);
-                  done();
-                });
-              });
+              done();
             });
           });
         });
       });
     }
 
-    function cleanupCreatedArtifacts(done)
-    {
-      suite.execute('ad app delete --objectId %s --quiet --json', aadObjectId, function (result) {
+    function cleanupCreatedArtifacts(done) {
+      suite.execute('keyvault delete --vault-name "%s" --quiet --json', vaultName, function (result) {
         result.exitStatus.should.equal(0);
-        suite.execute('keyvault delete -u %s --quiet --json', vaultName, function (result) {
+        suite.execute('group delete --name "%s" --quiet --json', groupName, function (result) {
           result.exitStatus.should.equal(0);
-          suite.execute('group delete -n %s --quiet --json', groupName, function (result) {
-            result.exitStatus.should.equal(0);
-            done();
-          });
+          done();
         });
       });
     }
 
-    describe('vm', function() {
-      it('should show the correct encryption status for the new Windows VM - not encrypted', function(done) {
-        var cmd = util.format('vm show-disk-encryption-status %s %s --json', groupName, vmPrefix).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.equal(0);
-          var allResources = JSON.parse(result.text);
-          allResources.osVolumeEncrypted.should.equal('NotEncrypted');
-          allResources.dataVolumesEncrypted.should.equal('NotEncrypted');
-          done();
-        });
-      });
-
-      it('should deserialize and then fail on invalid thumbprint when client secret not specified', function(done) {
-        var cmd = util.format('vm enable-disk-encryption -g %s -n %s -a %s -c %s -k %s -r %s --quiet --json', groupName, vmPrefix, spn, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', diskEncryptionKeySecretUrl, diskEncryptionKeyVaultId).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.not.equal(0);
-	  // presence of this error text implies that extension did not fail earlier at the deserialization stage 
-          should(result.errorText.indexOf('Cannot find AAD client certificate with thumprint') > -1).ok;
-          done();
-        });
-      });
-
-      it('should enable encryption on the Windows VM', function(done) {
-        var cmd = util.format('vm enable-disk-encryption -g %s -n %s -a %s -p %s -k %s -r %s --quiet --json', groupName, vmPrefix, spn, aadClientSecret, diskEncryptionKeySecretUrl, diskEncryptionKeyVaultId).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
+    describe('vm', function () {
+      it('should enable encryption on the vm', function (done) {
+        var cmd = util.format('vm enable-disk-encryption --resource-group "%s" --name "%s" --aad-client-id "%s" --aad-client-secret "%s" --disk-encryption-key-vault-url "%s" --disk-encryption-key-vault-id "%s" --quiet --json', groupName, vmName, adServicePrincipalAppId, adAppClientSecret, diskEncryptionKeyVaultUrl, diskEncryptionKeyVaultId).split(' ');
+        testUtils.executeCommand(suite, retry, cmd, function (result) {
           result.exitStatus.should.equal(0);
           done();
         });
       });
-
-      it('should show the correct encryption status for the VM - encrypted', function(done) {
-        var cmd = util.format('vm show-disk-encryption-status %s %s --json', groupName, vmPrefix).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.equal(0);
-          var allResources = JSON.parse(result.text);
-          allResources.osVolumeEncrypted.should.equal('Encrypted');
-          allResources.dataVolumesEncrypted.should.equal('Encrypted');
-          done();
-        });
-      });
-
-      it('should disable encryption on the Windows VM', function(done) {
-        var cmd = util.format('vm disable-disk-encryption -g %s -n %s --quiet --json', groupName, vmPrefix).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.equal(0);
-          done();
-        });
-      });
-
-      it('should show the correct encryption status for the VM - not encrypted', function(done) {
-        var cmd = util.format('vm show-disk-encryption-status %s %s --json', groupName, vmPrefix).split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.equal(0);
-          var allResources = JSON.parse(result.text);
-          allResources.osVolumeEncrypted.should.equal('NotEncrypted');
-          allResources.dataVolumesEncrypted.should.equal('NotEncrypted');
-          done();
-        });
-      });
-
     });
+
   });
 });
